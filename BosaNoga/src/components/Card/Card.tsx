e9ad20dce886cc636105bb6260289d8b16f30617
Renat Kalimulin <pianoparrot93@gmail.com>
2023-10-15 13:01:19 +0300

    Cards were aligned

--- BosaNoga/src/components/Card/Card.tsx
+++ BosaNoga/src/components/Card/Card.tsx
@@ -8,10 +8,26 @@ type CardProps = {
 
 function Card ({ item, isCatalogItem }: CardProps): JSX.Element {
   return (
-        <div className="col-4">
-            <div className={isCatalogItem ? 'card catalog-item-card' : 'card'}>
+        <div className="col-4 py-2">
+            <div className={isCatalogItem ? 'card catalog-item-card h-100' : 'card h-100'}>
                 <img src={item.images[0]}
                     className="card-img-top img-fluid" alt={item.title} />
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
+                <div className="spanner"></div>
                 <div className="card-body">
                     <p className="card-text">{item.title}</p>
                     <p className="card-text">{item.price} руб.</p>
--- BosaNoga/src/components/NoData/NoData.tsx
+++ BosaNoga/src/components/NoData/NoData.tsx
@@ -8,7 +8,7 @@ function NoData() {
             <div className="no-data-container text-center">
                 <img src={noDataImg.default} alt="no-data" className="no-data-img" />
                 <div className="no-data-layer"></div>
-                <p className='no-data-text'>Товары не найдены</p>
+                <p className='no-data-text'>Товары не найдены :-(</p>
             </div>
         </>
     )
